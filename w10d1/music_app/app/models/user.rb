ce54dae02a0afa31c179508bccc08ec078584080
Philippe Fonzin <pfonzin@gmail.com>
2020-01-27 13:24:13 -0500

    Created users table and user model

---
+++ w10d1/music_app/app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ApplicationRecord
+
+    validates :email, :password_digest, :session_token, presence: true 
+    validates :password_digest, length: { minimum: 6, allow_nil: true }  
+    after_initialize :ensure_session_token!
+    attr_reader :password 
+
+    def self.find_by_cred(username, password)
+        email = User.find_by(email: email) 
+        email && valid_password?(password) ? email : nil 
+    end 
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password) 
+    end 
+
+    def valid_password?(password)
+        Bcrypt::Password.new(password_digest).is_password?(password) 
+    end 
+
+    def reset_session_token
+        self.session_token = SecureRandom.url_safe.base64 
+        self.save
+        self.session_token
+    end 
+
+    private 
+    def ensure_session_token!
+        self.session_token ||= SecureRandom.url_safe.base64 
+    end 
+end
