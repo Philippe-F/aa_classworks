10e806a1cdbf4ff63870718eaff4890ecf0833e7
Philippe Fonzin <pfonzin@gmail.com>
2020-02-05 21:12:51 -0500

    Completed part of reversi project

--- w11d2/intro_to_js/phase_4_recursion.js
+++ w11d2/intro_to_js/phase_4_recursion.js
@@ -1,14 +1,28 @@
 const range = function range(start,end) {
   let result = [];
 
-  // debugger;
-
   if (start == end) {
     return [end]
   } else {
     result.push(start);
     result = result.concat(range(start + 1, end))
-  }
+  };
 
   return result;
 };
+
+function sumRec(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < this.length; i++) {
+    if (i === arr.length - 1) {
+      return sum 
+    } else {
+      sum += arr[i];
+      sumRec(arr.slice(i, arr.length - 1)) 
+    };
+  }; 
+
+};
+
+sumRec([1,2,3]) 
